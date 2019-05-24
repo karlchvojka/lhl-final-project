fa7bb6b0311f5fa163804746fa44c1ec800dc024
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-05-24 15:37:57 -0400

    work in progress

--- client/src/components/views/Home/home-nav-bar.js
+++ client/src/components/views/Home/home-nav-bar.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
+import Logo from "../../../assets/Home/feather.svg";
+import HomeImage from "../../../assets/Home/Img.svg";
 class NavbarComp extends Component {
   render() {
     return (
-        <Navbar expand="md">
-          <div className="container">
+      <Navbar expand="md">
+        <div className="container">
+          <img alt="" src={Logo} />
           <Navbar.Brand href="#home">Squabble</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Link className="nav-link" to="/">Home</Link>
-              <Link className="nav-link" to="/about">About</Link>
-              <Link className="nav-link" to="/login">Login</Link>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
             </Nav>
           </Navbar.Collapse>
-          </div>
-        </Navbar>
-    )
+        </div>
+      </Navbar>
+    );
   }
 }
-
 
 export default NavbarComp;
--- client/src/components/views/Home/index.js
+++ client/src/components/views/Home/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import ListApp from "../../apitest.js";
+import HomeImage from "../../../assets/Home/Img.svg";
 // test
 export default ({}) => {
   return (
     <main className="content-area container">
       <h1>THIS IS SQUABBLE!!!!!!!</h1>
-      <img src="https://media0.giphy.com/media/wegdgVEeRDEY0/giphy.gif" />
-      <ListApp />
+      <img className="renting-with-roommates-trio-of" alt="" src={HomeImage} />
     </main>
   );
 };
