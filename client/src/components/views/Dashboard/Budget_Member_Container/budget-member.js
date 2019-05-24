3423870b53fd9c3ec4eb7c66b454fb02ad255954
Karl <karl.chvojka@gmail.com>
2019-05-24 12:55:15 -0400

    Added folders for assets

---
+++ client/src/components/views/Dashboard/Budget_Member_Container/budget-member.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BudgetMember extends Component {
+  render() {
+    return (
+      <div>
+        <p>Budget Member Component</p>
+      </div>
+      )
+    }
+  }
+
+  export default BudgetMember;
