e18b41d2fdd7c921b5009be9f389642340276e6c
Eden Yeung <basktballer@gmail.com>
2019-05-24 18:56:38 +0000

    Sign up feature front end skeleton, minor refactoring to home, home-nav-bar, App.js, card.js, apitest.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,46 @@
 // React imports
-import React from 'react';
+import React, { Component } from 'react';
 import ListApp from "./components/apitest.js"
-import NavbarComp from "./components/views/Home/home-nav-bar.js"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/views/Home/';
 import Aboutpg from './components/views/About/';
 import HomeFooter from './components/views/footer.js'
 import Login from './components/views/Login/';
+import Signup from './components/views/Signup'
 
 // CSS Imports
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 
-function App() {
-  return (
-    <Router>
-    <div className="container-fluid">
-      <NavbarComp />
-      <Route path="/" exact component={ Home } />
-      <Route path="/about" exact component={ Aboutpg  } />
-      <Route path="/login" component={ Login } />
-      <HomeFooter />
-    </div>
-    </Router>
-  );
-}
+export default class App extends Component { 
 
-export default App;
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+
+  handleSignupFormSubmit = evt => {
+    evt.preventDefault();
+    console.log("These are the signup form values", evt.target.first.value, evt.target.last.value, evt.target.email.value, 
+    evt.target.password.value, evt.target.passwordconf.value, evt.target.phone.value, evt.target.photo.value);
+  }
+
+  
+ render() {
+    return (
+      <Router>
+        <div className="container-fluid">
+        <Route path="/" exact component={ Home } />
+        <Route path="/about" exact component={ Aboutpg  } />
+        <Route path="/login" component={ Login } />
+        {/* <Route path="/signup" component={ Signup } /> */}
+        <Route path="/signup" component={() => <Signup handleFormSubmit={this.handleSignupFormSubmit}/> } />
+        <HomeFooter />
+        </div>
+      </Router>
+      );
+    }
+    
+  }
