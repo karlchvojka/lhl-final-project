21a5505e528edfd447a36a70feffefc26e45924f
Karl <karl.chvojka@gmail.com>
2019-05-24 15:02:17 -0400

    Login page structure completed

--- client/src/components/views/Login/index.js
+++ client/src/components/views/Login/index.js
@@ -2,12 +2,52 @@ import React, { Component } from 'react';
 import NavbarLoginSignup from '../login-signup-navbar.js'
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {firstName: '', lastName: '', email: '', password: ''};
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.email.value,event.target.password.value )
+  }
+
   render() {
     return (
       <div className="container-fluid loginPage">
-      <NavbarLoginSignup />
-        <p>login page</p>
-      </div>
+        <NavbarLoginSignup />
+
+        <div className="row loginForm">
+          <div className="col-6 offset-3">
+            <h1>Signup</h1>
+            <form onSubmit={this.handleSubmit}>
+              <div className="container">
+                <div className="row">
+                  <div className="col-12">
+                    <label>
+                      Email:
+                      <input type="email" name="email"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12">
+                  <label>
+                    Password:
+                    <input type="text" name="password" />
+                  </label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12">
+                    <input type="submit" value="Submit" />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        </div>
       )
     }
   }
