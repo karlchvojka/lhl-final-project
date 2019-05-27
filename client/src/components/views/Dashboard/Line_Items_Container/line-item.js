f68362d8a48686f654a00174cbaad0ac38476c04
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-05-27 13:18:57 -0400

    stashing 2

--- client/src/components/views/Dashboard/Line_Items_Container/line-item.js
+++ client/src/components/views/Dashboard/Line_Items_Container/line-item.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class LineItem extends Component {
   render() {
     return (
       <div>
-        <p>{this.props.name}: {this.props.price}</p>
+        <p>
+          {this.props.line_item_creator}: {this.props.name}:{" $"}
+          {(this.props.price / this.props.total_members).toFixed(2)}
+        </p>
       </div>
-      )
-    }
+    );
   }
+}
 
-  export default LineItem;
+export default LineItem;
