73989b8a2b69d631b52ae685c1551d1512b093fe
Karl <karl.chvojka@gmail.com>
2019-05-31 17:10:55 -0400

    Finished dashboard

--- client/src/components/views/Dashboard/budgetCard/index.js
+++ client/src/components/views/Dashboard/budgetCard/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 class BudgetCard extends Component {
-
+  render() {
+    return(
+      <Col className="budgetCardWrap" xl={4} lg={4} md={4} sm={2} xs={2}>
+        <Card>
+          <Card.Img variant="top" src="" />
+          <Card.Body>
+            <Card.Title>{this.props.budgetName}</Card.Title>
+            <Card.Text>
+              This is a BUDGET!!!!!!
+            </Card.Text>
+            <Button variant="primary" href="/budget">Go to Budget</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  }
 }
 
 export default BudgetCard;
